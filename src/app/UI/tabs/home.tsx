97fc5754e73ac1ba795b9637e0351451f5a61542
Diego Serrat <31071378+diegoserrat@users.noreply.github.com>
2025-01-07 19:50:52 -0300

    adding cards to home page, and linking then with the championship page

--- src/app/UI/tabs/home.tsx
+++ src/app/UI/tabs/home.tsx
@@ -1,9 +1,37 @@
-import { View, Text, StyleSheet } from "react-native";
+import { fetchAllChampionships } from "@/app/service/service-championship";
+import COLORS from "@/app/shared/utils/colors";
+import { useQuery } from "@tanstack/react-query";
+import { Image } from "expo-image";
+import { Link } from "expo-router";
+import React, { useEffect } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 export default function Home() {
+
+    const { data, isLoading, error} = useQuery({
+        queryKey: ['championships'],
+        queryFn: () => fetchAllChampionships(),
+    });
+
     return (
+            
         <View style={styles.container}>
-            <Text>Home tabs</Text>
+            { isLoading ? (
+                <Text>Carregando...</Text>
+            ) : 
+                <>
+                    {data && data?.map((item: any) => (
+                        <View key={item.id} style={styles.cardChampionship}>
+                            <Link href={{ pathname: './championship', params: { id: item.id }}} asChild>
+                                <TouchableOpacity onPress={() => {}}>
+                                    <Text>{JSON.parse(item.jsonFromExternalApi).edicao_atual.nome_popular}</Text>
+                                    <Image source={{ uri: JSON.parse(item.jsonFromExternalApi).edicao_atual.logo }} />
+                                </TouchableOpacity>
+                            </Link>
+                        </View>
+                    ))}
+                </> 
+            }
         </View>
     );
 }
@@ -13,6 +41,23 @@ const styles = StyleSheet.create({
       flex: 1,
       width: '100%',
       padding: 16,
-      backgroundColor:'#FFFFFF',
+      backgroundColor: COLORS.white,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    cardChampionship: {
+        width: '100%',
+        height: 100,
+        padding: 16,
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 16,
+        borderWidth: 1,
+        borderColor: COLORS.gray,
+        borderRadius: 5,
+        shadowColor: COLORS.black,
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5
     }
 });
--- src/app/shared/utils/colors.ts
+++ src/app/shared/utils/colors.ts
@@ -7,7 +7,7 @@ const COLORS = {
     black: '#000000',
     light_green: '#04bacf',
     gray: '#CCCCCC',
-    light_gray: '#f4f4f4'
+    light_gray: '#f4f4f4',
 };
   
 export default COLORS;
